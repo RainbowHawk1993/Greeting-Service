417272dfc9a34228bd4a312f95206d3d1d595b75
Nick <nicklos550@gmail.com>
2022-01-06 00:34:03 +0200

    fixed a bug that allowed duplicate e-mails

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from datetime import date
 
 app = Flask(__name__)
 
+emails_to_display = []
 emails = []
 
 @app.route('/')
@@ -24,10 +25,11 @@ def greeting():
            #error_statement = "Привіт", email, "вже бачилися!"
             error_statement = "Привіт {} вже бачилися!".format(email)
             return render_template("index.html", error_statement=error_statement, email=email)
-
-    emails.append(f" {email}   ||  {today}")
+            
+    emails.append(email)
+    emails_to_display.append(f" {email}   ||  {today}")
     return render_template("greeting.html", email=email)
 
 @app.route('/list')
 def list():
-    return render_template("list.html", emails=emails)
+    return render_template("list.html", emails=emails_to_display)
